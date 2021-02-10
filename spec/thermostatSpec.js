ac3772641ef824d900b7a7ae3a8cbbc7453ff187
Dan T <dp.tyler@hotmail.co.uk>
2021-02-10 16:59:34 +0000

    savemode added with no magic numbers

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,25 +7,43 @@ describe('Thermostat', function() {
   });
 
   it('starts at 20 degrees', function() {
-    expect(thermostat._temp).toEqual(20);
+    expect(thermostat._temp).toEqual(DEFAULT_TEMP);
   });
 
   it('should be able to increase the temperature', function() {
     thermostat.increaseTemp();
-    expect(thermostat._temp).toEqual(21);
+    expect(thermostat._temp).toEqual(DEFAULT_TEMP + 1);
   });
 
   it('should be able to decrease the temperature', function() {
     thermostat.decreaseTemp();
-    expect(thermostat._temp).toEqual(19);
+    expect(thermostat._temp).toEqual(DEFAULT_TEMP - 1);
   });
 
   it('should throw an error if temp is decreased below 10', function() {
     let i = 0;
-    while(i++ < 10) {
+    while(i++ < DEFAULT_TEMP - MIN_TEMP) {
       thermostat.decreaseTemp();
     }
     expect(function() {thermostat.decreaseTemp()}).toThrowError('Cannot exceed min. temperature');
+  });
+
+  it('should have a maxTemp of 25 in default', function() {
+    expect(thermostat._maxTemp).toEqual(SAVE_MODE_MAX);
+    expect(thermostat._saveStatus).toBe(true);
+  });
+
+  it('should have a maxTemp of 32 when power save mode is off', function() {
+    thermostat.saveModeOff();
+    expect(thermostat._maxTemp).toEqual(SAVE_MODE_OFF_MAX);
+    expect(thermostat._saveStatus).toBe(false);
+  });
+
+  it('should be able to turn power save mode on', function() {
+    thermostat.saveModeOff();
+    thermostat.saveModeOn();
+    expect(thermostat._maxTemp).toEqual(SAVE_MODE_MAX);
+    expect(thermostat._saveStatus).toBe(true);
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,9 +1,17 @@
 'use strict';
 
+const SAVE_MODE_MAX = 25;
+const SAVE_MODE_OFF_MAX = 32;
+const DEFAULT_TEMP = 20;
+const MIN_TEMP = 10;
+
 class Thermostat {
+  
   constructor() {
-    this._temp = 20;
-    this._MINIMUM_TEMP = 10;
+    this._temp = DEFAULT_TEMP;
+    this._MINIMUM_TEMP = MIN_TEMP;
+    this._maxTemp = SAVE_MODE_MAX;
+    this._saveStatus = true 
   }
   
   increaseTemp() {
@@ -16,4 +24,15 @@ class Thermostat {
     }
     this._temp--;
   }
+
+  saveModeOff() {
+    this._maxTemp = SAVE_MODE_OFF_MAX;
+    this._saveStatus = false;
+  }
+
+  saveModeOn() {
+    this._maxTemp = SAVE_MODE_MAX;
+    this._saveStatus = true;
+  }
+ 
 }
